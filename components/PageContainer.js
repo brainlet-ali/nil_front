67c123863b9351b3c5d13dbd75e0c0601ba676a9
Ali Raza <brainlet.ali@gmail.com>
2022-07-02 16:56:49 +0500

    wip

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -1,7 +1,7 @@
-export const PageContainer = props => {
+export const PageContainer = (props) => {
   return (
-     <div className="container mx-auto sm:px-6 lg:px-8 py-10">
-       {props.children}
-     </div>
-   );
+    <div className="container mx-auto h-full max-w-7xl py-10 sm:px-6 lg:px-8">
+      {props.children}
+    </div>
+  )
 }
