5b5b7c1e6b771ceab78b57f54b952ef4236c3029
Ali Raza <brainlet.ali@gmail.com>
2022-07-09 12:34:33 +0500

    wip

--- components/feature/FeatureLoading.js
+++ components/feature/FeatureLoading.js
@@ -1,5 +1,3 @@
-import CodeBlock from '@/components/inc/CodeBlock'
-
 export default function FeatureLoading(props) {
   return (
     <div className={'animate-pulse'}>
@@ -8,15 +6,14 @@ export default function FeatureLoading(props) {
           'flex flex-col items-center justify-between space-y-4 text-center sm:flex-row sm:space-y-0'
         }
       >
-        <div className="h-4 w-3/5 rounded-xl bg-slate-200"></div>
+        <div className="h-4 w-3/5 rounded-xl bg-zinc-200 dark:bg-zinc-500"></div>
         <span>
-          PR&nbsp;&nbsp;
-          <span className="inline-block h-4 w-20 rounded-xl bg-slate-200"></span>
+          <span className="inline-block h-4 w-20 rounded-xl bg-zinc-200 dark:bg-zinc-500"></span>
         </span>
       </div>
-      <div className="my-2 mx-auto h-4 w-4/5 rounded-xl bg-slate-200 sm:mx-0"></div>
+      <div className="my-2 mx-auto h-4 w-4/5 rounded-xl bg-zinc-200 dark:bg-zinc-500 sm:mx-0"></div>
 
-      <div className="mt-4 h-48 rounded-xl bg-slate-200"></div>
+      <div className="mt-4 h-48 rounded-xl bg-zinc-200 dark:bg-zinc-500"></div>
     </div>
   )
 }
