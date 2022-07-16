6f8924935a0ae6d6f3a195c8572273b262d351d2
Ali <ali@brainlet.co>
2022-07-16 07:41:58 +0500

    wip

--- components/inc/Nav.js
+++ components/inc/Nav.js
@@ -5,10 +5,15 @@ export default function Nav() {
     <nav className={'space-x-6'}>
       <Link href={'/'}>
         <a className={'tracking-widest text-brand hover:text-brand-700'}>
+          Home
+        </a>
+      </Link>
+      <Link href={'/about'}>
+        <a className={'tracking-widest text-brand hover:text-brand-700'}>
           About
         </a>
       </Link>
-      <Link href={'/'}>
+      <Link href={'/why'}>
         <a className={'tracking-widest text-brand hover:text-brand-700'}>Why</a>
       </Link>
     </nav>
